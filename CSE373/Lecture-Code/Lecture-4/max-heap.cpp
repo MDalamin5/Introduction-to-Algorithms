3778399bdfbd660f7b09d51acd854ff27215fc56
MDalamin5 <mdal.amin5@northsouth.edu>
2024-04-01 21:56:21 +0600

    heap insert and up heapipy done

--- CSE373/Lecture-Code/Lecture-4/max-heap.cpp
+++ CSE373/Lecture-Code/Lecture-4/max-heap.cpp
@@ -1,5 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
+class MaxHeap{
+public:
+    vector<int>nodes;
+    MaxHeap(){
+
+    }
+    void upHeapify(int indx){
+        while(indx > 0 && nodes[(indx-1)/2] < nodes[indx]){
+            swap(nodes[(indx-1)/2] , nodes[indx]);
+            indx = (indx-1)/2;
+        }
+    }
+    void insert(int x){
+        nodes.push_back(x);
+        upHeapify(nodes.size() - 1);
+    }
+
+    void DisplayHeap(){
+        for(int i=0; i < nodes.size(); i++){
+            cout<<nodes[i]<<" ";
+        }
+    }
+};
 int main(){
+    MaxHeap heap;
+    heap.insert(4);
+    heap.insert(7);
+    heap.insert(9);
+    heap.insert(1);
+    heap.insert(10);
     
+    heap.DisplayHeap();
 }
