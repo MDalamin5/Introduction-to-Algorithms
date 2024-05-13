4a81189c20e736b3e90ad0732e52e1e04d355135
MDalamin5 <mdal.amin5@northsouth.edu>
2024-05-13 10:41:48 +0600

    update quick sort

--- CSE373/Assignments/Assignment-1/quickSoet.py
+++ CSE373/Assignments/Assignment-1/quickSoet.py
@@ -1,3 +1,6 @@
+import random
+import time
+
 def Partition(A, p, r):
     x = A[r]
     i = p - 1
@@ -21,19 +24,19 @@ def QuickSort(A, p, r):
 
 
 # A = [2, 5, 8, 3, 9, 4, 1, 7, 10, 6]
-import random
-import time
+
 
 # Record the start time
 
 
 
-A = [random.randint(0, 1500) for _ in range(1000)]
+A = [random.randint(0, 150) for _ in range(50)]
 # print(random_numbers)
 start_time = time.time()
 QuickSort(A, 0, len(A)-1)
 end_time = time.time()
-
+print(A)
 execution_time = end_time - start_time
+print(execution_time)
 # print(A)
 
