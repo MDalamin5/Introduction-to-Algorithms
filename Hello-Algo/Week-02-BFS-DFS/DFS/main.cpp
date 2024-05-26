6f876b7d9b4b953486473c8ebfd031627a0c81ff
MDalamin5 <mdal.amin5@northsouth.edu>
2024-05-26 17:48:15 +0600

    implement bfs code in cpp

--- Hello-Algo/Week-02-BFS-DFS/DFS/main.cpp
+++ Hello-Algo/Week-02-BFS-DFS/DFS/main.cpp
@@ -1,17 +1,49 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 const int N = 1e3;
 int visited[N];
-
 vector<int> adj_list[N];
 
-void dfs(int node){
-    cout<<node<<endl;
+void dfs(int node) {
+    cout << node << endl;
+    visited[node] = 1;
 
-    for(int adj_node: adj_list[N]){
-        if(visited[adj_node] == 0){
+    for (int adj_node : adj_list[node]) {
+        if (visited[adj_node] == 0) {
             dfs(adj_node);
         }
     }
+}
+
+/*
+0-----1-------2--------3
+      |       \
+      \       \
+      5 ------4 
+
+6 6
+0 1
+2 3
+1 5
+2 4
+5 4
+1 2   
+
+*/
+
+int main() {
+    int nodes, edges;
+    cin >> nodes >> edges;
+
+    for (int i = 0; i < edges; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj_list[u].push_back(v);
+        adj_list[v].push_back(u);
+    }
+
+    int srcNode = 0;
+    cout<<"the output is: "<<endl;
+    dfs(srcNode);
 }
